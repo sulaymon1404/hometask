0208c7a588294007b3a60d04fe645cc44d5fc80a
Sulaymon <sulaymonmakhsumow@gmail.com>
2023-01-20 18:29:29 +0300

    add home tasks

---
+++ index.js
@@ -0,0 +1,68 @@
+// FUNCTION
+
+// task1
+
+// function pow(a){
+//     return Math.pow(a,2)
+// }
+// console.log(pow(4))
+
+
+
+
+// task2
+
+// function isSameNum(a,b){
+//     if(a===b){
+//         return ("true")
+//     }
+//     else{
+//         return ("false")
+//     }
+// }
+
+// console.log(isSameNum(2,4))
+
+// task3
+
+// let a=prompt()
+// let cnt=0
+// function findNemo(a){
+//     a=a.split(" ")
+//     for(let i=0;i<a.length;i++){
+//         if(a[i]!="Nemo"){
+//             cnt++
+//         }
+//         else{
+//             break
+//         }
+//     }
+//     return cnt+1
+// }
+// console.log("I fount Nemo at"+" "+ findNemo(a))
+
+
+// task4
+
+// let a=prompt()
+
+// function add(a){
+//     return a.replaceAll(" ","R")
+// }
+
+// console.log(add(a))
+
+
+// task5
+
+// let a=prompt()
+
+// function hacker(a){
+//     a=a.replaceAll("e","3")
+//     a=a.replaceAll("a","4")
+//     a=a.replaceAll("o","0")
+//     a=a.replaceAll("i","1")
+//     a=a.replaceAll("s","5")
+//    return a
+// }
+// console.log(hacker(a))
